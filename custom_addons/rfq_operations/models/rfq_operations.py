4cf1f5ac99b810a7ef3aac02edc13134c03ebc9b
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-17 14:19:36 +0300

    feat: ability to assign rfq to vendors

--- custom_addons/rfq_operations/models/rfq_operations.py
+++ custom_addons/rfq_operations/models/rfq_operations.py
@@ -4,11 +4,15 @@ from odoo import fields, models
 
 class RFQOperations(models.Model):
     _inherit = 'purchase.order'
+#
+    vendor_ids = fields.One2many('rfq.assigned', 'rfq_assigned_id', string='Vendors')
 
-    vendor_ids = fields.One2many('res.partner', 'rfq_id', string='Vendors')
 
 
 class RFQAssigned(models.Model):
-    _inherit = 'res.partner'
+    _name ="rfq.assigned"
+    _description = "RFQ Assign To Vendors"
 
-    rfq_id = fields.Many2one('purchase.order', string='RFQ Assigned')
+    rfq_assigned_id = fields.Many2one('purchase.order', string='RFQ Assigned')
+    vendor_name = fields.Char(string='Vendor')
+
