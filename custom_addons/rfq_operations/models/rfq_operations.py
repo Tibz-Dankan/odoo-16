2ec5a2ceec3d0096f91a942c2e1be2390061dfa5
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-22 11:06:09 +0300

    feat: add many2many_tags to vendors of the rfq

--- custom_addons/rfq_operations/models/rfq_operations.py
+++ custom_addons/rfq_operations/models/rfq_operations.py
@@ -3,16 +3,14 @@
 from odoo import fields, models
 
 class RFQOperations(models.Model):
-    _inherit = 'purchase.order'
-#
-    vendor_ids = fields.One2many('rfq.assigned', 'rfq_assigned_id', string='Vendors')
+    _inherit = 'res.partner'
+
+    # rfq_assigned = fields.Many2many("purchase.order", "VendorsAssigned")
+    # vendor_ids = fields.One2many('res.partner', 'rfq_assigned_id', string='Vendors')
 
 
 
 class RFQAssigned(models.Model):
-    _name ="rfq.assigned"
-    _description = "RFQ Assign To Vendors"
+    _inherit = 'purchase.order'
 
-    rfq_assigned_id = fields.Many2one('purchase.order', string='RFQ Assigned')
-    vendor_name = fields.Char(string='Vendor')
-
+    vendor_ids = fields.Many2many("res.partner.category", "purchase_order_tag_rel", "purchase_id", "tag_id", "Vendors")
